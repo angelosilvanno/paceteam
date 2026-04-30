28ec126603047edf704d65e4cf309dc00aea3c18
Ângelo <familiasabino14@gmail.com>
2026-04-30 16:41:36 -0300

    feat: implementa lógica de autenticação na navbar com exibição condicional de links

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router'; // Adicione este import
+import { Component, inject } from '@angular/core'; // Adicione o inject aqui
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule], // Adicione aqui
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar { }
+export class Navbar {
+  // Em vez de colocar no constructor, injetamos direto na variável
+  private authService = inject(AuthService);
+  
+  // Agora o user$ consegue ler o serviço sem erro
+  user$ = this.authService.user$;
+
+  logout() {
+    this.authService.logout();
+  }
+}
